2cd876bce645d978e934a5c12ad42ded1e822992
fidelis27 <thiago.fidelis27@gmail.com>
2019-08-14 19:51:58 -0300

    .env

--- db.js
+++ db.js
@@ -5,4 +5,4 @@ const mongoose = require('mongoose')
 /* mongoose.connect('mongodb+srv://thiago:thiago27@cluster0-6lenz.mongodb.net/gerenciadorSeries?retryWrites=true&w=majority',{ useNewUrlParser: true } ) */
 
 
- mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true}) 
+ mongoose.connect('process.env.MONGO_URL', {useNewUrlParser: true}) 
