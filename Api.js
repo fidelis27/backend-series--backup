6fd12457ec5693ac102f3debe387c170e8a039e0
fidelis27 <45233696+fidelis27@users.noreply.github.com>
2019-08-28 15:27:45 -0300

    Update Api.js

--- Api.js
+++ Api.js
@@ -17,6 +17,7 @@ server.use(express.text())
 server.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
     next()
   })
 server.use(routesGenres)
